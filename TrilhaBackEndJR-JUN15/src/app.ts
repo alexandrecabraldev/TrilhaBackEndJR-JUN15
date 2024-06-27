2c28c6de3504f7160f03eb0b2e50703cbc4de84b
Alexandre Cabral <alexandre_c.m@hotmail.com>
2024-06-27 20:56:40 -0300

    in app, add /login, /user, /task routes and dotenvConfig add more variables on schema

--- TrilhaBackEndJR-JUN15/src/app.ts
+++ TrilhaBackEndJR-JUN15/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express"
 import { userRouter } from "./routes/user.routes";
 import { env } from "./dotenvConfig";
+import { loginRouter } from "./routes/login.routes";
+import { taskRoute } from "./routes/task.routes";
 
 const app = express();
 app.use(express.json());
 
+app.use('/login', loginRouter)
 app.use('/user', userRouter)
+app.use('/task', taskRoute)
 
 
 app.listen(env.PORT,()=>{
--- TrilhaBackEndJR-JUN15/src/dotenvConfig.ts
+++ TrilhaBackEndJR-JUN15/src/dotenvConfig.ts
@@ -2,7 +2,9 @@ import "dotenv/config"
 import z from "zod"
 
 const dotenvSchema = z.object({
-    PORT: z.coerce.number().default(3000)
+    PORT: z.coerce.number().default(3000),
+    SECRET_KEY: z.string(),
+    DATABASE_URL: z.string(),
 })
 
 const _env = dotenvSchema.safeParse(process.env)
